872386f7cb62fd90cf58a01fd918b28682732ee2
Ubuntu <ubuntu@ip-172-31-11-30.ec2.internal>
2020-11-20 02:52:23 +0000

    added hello_world.cpp with proper style

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2020 Marlon Poddalgoda, All rights reserved
+//
+// Created by Marlon Poddalgoda
+// Created on November 19 2020
+// This is a "Hello, World!" program with proper style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
